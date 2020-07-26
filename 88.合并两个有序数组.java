6b768a4973e8d0d37a965048eb1f49a23f500f8b
sev7e0 <sev7e0@gmail.com>
2020-07-26 21:31:35 +0800

    [array]合并两个有序数组

--- 88.合并两个有序数组.java
+++ 88.合并两个有序数组.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 // @lc code=start
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+
         m = m - 1;
         n = n - 1;
         for (int i = nums1.length - 1; i >= 0; i--) {
@@ -23,6 +24,29 @@ class Solution {
 
         }
         System.arraycopy(nums2, 0, nums1, 0, n + 1);
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+
+        // int[] newNums = Arrays.copyOf(nums1, m);
+        // int mIndex = 0;
+        // int nIndex = 0;
+        // for (int i = 0; i < nums1.length; i++) {
+        //     // 当nums1全部排序完成后，将nums2剩余都放到数组中
+        //     if (mIndex == m) {
+        //         System.arraycopy(nums2, nIndex, nums1, i, n - nIndex);
+        //         break;
+        //     }
+        //     if (nIndex == n) {
+        //         System.arraycopy(newNums, mIndex, nums1, i, m - mIndex);
+        //         break;
+        //     }
+        //     if (newNums[mIndex] >= nums2[nIndex]) {
+        //         nums1[i] = nums2[nIndex];
+        //         nIndex++;
+        //     } else {
+        //         nums1[i] = newNums[mIndex];
+        //         mIndex++;
+        //     }
+        // }
     }
 }
 // @lc code=end
